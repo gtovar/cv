673bc748b3d36b58040cc449220db47aa4e9534d
gtovar1985 <gtovar@incaztech.com>
2014-06-09 00:03:28 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gilbertotovar::Application.config.secret_key_base = 'eb3cf4dc4694d8b3d432f2e4fd5fb758f3e303fde6e5ef7c298010226039f3c265b63de4821e9c351a9d389e7d64155d7d091b4acba3a1bf1fb0514548835ca3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gilbertotovar::Application.config.session_store :cookie_store, key: '_gilbertotovar_session'
