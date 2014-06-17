b1e300c1bcf8438d5a47c98f4da463d5721782e5
gtovar1985 <gtovar@incaztech.com>
2014-06-16 23:28:13 -0500

    Add new strong params for new field

--- app/models/proyect.rb
+++ app/models/proyect.rb
@@ -2,9 +2,10 @@ class Proyect < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: [:slugged,:history]
 
-  translates :position, :activities, :outstaning, :description, :activity_description
+  translates :position, :activities, :outstaning, :description, :activity_description, :category
   mount_uploader :cover, CoverUploader
   mount_uploader :logo, LogoUploader
+  validates :name,:start_time,:end_time,:position,:activities,:outstaning,:tools,:description,:cover,:activity_description,:category, presence: true
   def should_generate_new_friendly_id?
       name_changed?
   end
