d024684897f04e034d72e2c3253e44c411ee1bc3
gtovar1985 <gtovar@incaztech.com>
2014-06-30 15:17:04 -0500

    Move into separete file for mor control and cleaner transalations

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Gilbertotovar
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales','**', '*.{rb,yml}')]
     config.i18n.default_locale = :es
     config.i18n.fallbacks = true
   end
