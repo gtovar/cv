0f84eecf4bbc1d13cfb060923ce6da19abc5dec9
Gilberto Tovar <tovarninogilberto@gmail.com>
2017-06-06 06:36:35 -0500

    Captcha READY

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,38 @@ class StaticPagesController < ApplicationController
 
   include ActionView::Helpers::TextHelper 
 
+  def send_email
+      unless params["email"].blank? 
+          puts "estamos dentro de params.blank #{params.inspect}"
+          @c = ContactForm.new(:name => params["name"],:email => params["email"], :message => params["message"])
+          if verify_recaptcha(model: @c,response:  params['g-recaptcha-response']) && @c.deliver
+              begin
+                  account_sid = ENV['TWILIO_ACCOUNT_SID']
+                  auth_token  = ENV['TWILIO_AUTH_TOKEN']
+                  client = Twilio::REST::Client.new account_sid, auth_token
+                  from = "+16096442390" # Your Twilio number
+                  friends =  "+5218113129230"
+                  client.account.messages.create(
+                      :from => from,
+                      :to => friends,
+                      :body => "Tienes un email de #{params["name"]} y dice, #{truncate(params["message"], length: 100)}"
+                  ) 
+                  puts "\n\n Sent message to #{friends}"
+              rescue Twilio::REST::RequestError => e
+                  puts "#{e.message} ERROR de Twilio"
+              end
+          else
+              @c.errors.add(:base, "Error captcha no paso la validacion")
+          end
+      end
+      respond_to do |format|
+          format.html {render :layout => false}
+      end
+  end
+  
   def home
-    @url_resume = download_resume
-    unless params.blank? 
-    end
+      params.permit(:"g-recaptcha-response")
+      @url_resume = download_resume
   end
 
 private
